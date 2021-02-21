603a957e80251c760074f5284cfc4f218edd4071
Muratbek3 <45931103+Muratbek3@users.noreply.github.com>
2021-02-21 09:59:18 +0300

    Delete code-online-store directory

--- code-online-store/js/src/view/filter-all-view.js
+++
@@ -1,9 +0,0 @@
-import {AbstractFilterView} from './abstract-filter-view.js';
-
-const createRangeFilterTemplate = () => `<div></div>`;
-
-export class FilterAllView extends AbstractFilterView {
-  getTemplate() {
-    return createRangeFilterTemplate();
-  }
-}
